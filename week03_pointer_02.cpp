3705a92000540b67e8541f35b69a4937fb2e18a6
Min <202130208@email.daelim.ac.kr>
2025-03-24 13:42:00 +0900

    초기 커밋.

---
+++ week03_pointer_02.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int totalPrice = 0;
+//	int people = 0;
+//	//int ages[people];
+//	//int ages = [people]이라 하면 오류가 발생함
+//	//가변 값이 들어가면 안되기때문
+//	cout << "몇 명입니까? ";
+//	cin >> people;
+//
+//	int* ages;
+//	ages = new int[people];//people의 값에 따라 변경됨 4명이면 4byte 메모리 4개 할당됨
+//
+//	for (int i = 0; i < people; i++) {
+//		cout << "나이를 입력하세요 : ";
+//		cin >> *(ages+i); // ages의 메모리에 할당함
+//	}
+//	for (int j = 0; j < people; j++) {
+//		// kid : 5000, adult : 9000 , senior : 6000
+//		/*cout << *(ages + j) << '\n';*/
+//		int age = *(ages + j);//힙 메모리에 할당된 값을 가져옴
+//		if (age < 8) {
+//			totalPrice += 5000;
+//		}
+//		else if (age < 65) {
+//			totalPrice += 9000;
+//		}
+//		else {
+//			totalPrice += 6000;
+//		}
+//	}
+//	cout << "전체 금액은 : " << totalPrice << "원 입니다.";
+//
+//	delete[] ages;
+//
+//
+//	return 0;
+//}
+//
+//
+//
+//
+//
---
+++ week03_pointer_04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 9;
+	int* pa = &a;
+	int** ppa = &pa;
+	cout << **ppa << '\n';// 이중포인터 출력
+	cout << &a << " / " << pa << " / " <<*ppa <<'\n';
+	cout << a << " / " << *pa << " / " << **ppa << '\n'; 
+	cout << &pa << " / " << ppa << '\n';//ppa = &pa 즉 pa의 주소값과 동일
+	cout << &ppa << '\n';
+	return 0;
+}
+
+
+
+
+
