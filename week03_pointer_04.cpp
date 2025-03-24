2c66600f197eca5186ce5511979bac75630a35dd
Min <202130208@email.daelim.ac.kr>
2025-03-24 13:44:57 +0900

    테스트커밋

--- week03_pointer_04.cpp
+++ week03_pointer_04.cpp
@@ -9,6 +9,7 @@ int main() {
 	cout << a << " / " << *pa << " / " << **ppa << '\n'; 
 	cout << &pa << " / " << ppa << '\n';//ppa = &pa 즉 pa의 주소값과 동일
 	cout << &ppa << '\n';
+	cout << "테스트입니다." << '\n';
 	return 0;
 }
 
